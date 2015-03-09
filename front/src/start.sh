1b8656abc3333dd0fd107d062be67bf939b82872
Thomas Clavier <tom@tcweb.org>
2015-03-09 13:47:24 +0100

    add git backend

--- front/src/start.sh
+++ front/src/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed -e "s/JENKINS_IP/$JENKINS_PORT_8080_TCP_ADDR/g"  -e "s/TOMCAT_IP/$TOMCAT_PORT_8080_TCP_ADDR/g"  -i /etc/nginx/conf.d/iut.azae.net.conf
+sed -e "s/GIT_IP/$GIT_PORT_80_TCP_ADDR/g" -i /etc/nginx/conf.d/iut.azae.net.conf
 
 /etc/init.d/nginx start
